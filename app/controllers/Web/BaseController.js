00e2894743c09479011b44de4b80de138365a7ef
Glenn de Haan <glenn@dehaan.cloud>
2018-09-04 13:00:28 +0200

    Fixed the assets regeneration

--- app/controllers/Web/BaseController.js
+++ app/controllers/Web/BaseController.js
@@ -22,12 +22,14 @@ class BaseController {
      * @param pageSpecificConfig
      */
     mergePageConfig(request, pageSpecificConfig) {
+        const files = assets();
+
         this.baseConfig.hostname = request.get('host');
         this.baseConfig.protocol = request.protocol;
         this.baseConfig.baseUrl = `${request.protocol}://${request.get('host')}${config.application.basePath}`;
 
-        this.baseConfig.assets.js = assets["main.js"];
-        this.baseConfig.assets.css = assets["main.css"];
+        this.baseConfig.assets.js = files["main.js"];
+        this.baseConfig.assets.css = files["main.css"];
 
         return Object.assign(this.baseConfig, pageSpecificConfig);
     }
--- app/helpers/modules/Assets.js
+++ app/helpers/modules/Assets.js
@@ -1,4 +1,6 @@
 const fs = require("fs");
 const path = `${__dirname}/../../../public/dist`;
 
-module.exports = JSON.parse(fs.existsSync(path) ? fs.readFileSync(`${path}/rev-manifest.json`) : "{}");
+module.exports = () => {
+    return JSON.parse(fs.existsSync(path) ? fs.readFileSync(`${path}/rev-manifest.json`) : "{}");
+};
